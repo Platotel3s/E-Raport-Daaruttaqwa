135ef063b5bc621cfe3085633ee8423edf88cbf7
yudha <yudhaplato@gmail.com>
2025-02-21 19:33:37 +0700

    Penambahan fitur pada wali kelas

--- resources/views/walas/main.blade.php
+++ resources/views/walas/main.blade.php
@@ -1,6 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Pusat Role walas</h1>
-    <p>Selamat datang Pak/Bu {{ Auth::user()->name }}</p>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h1>Pusat Role walas</h1>
+                    </div>
+                    <div class="card-body">
+                        <p>Selamat datang Pak/Bu {{ Auth::user()->name }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
